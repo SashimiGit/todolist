e6a9f3780f07f428f84e62d252937dfe695cf537
Kososhin <27874235+Kososhin@users.noreply.github.com>
2020-06-25 01:17:22 +0800

    wrote index.blade.html

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Folder;
+use App\Task;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
 
@@ -12,9 +13,14 @@ class TaskController extends Controller
     public function index(int $id){
         $folders = Folder::all();
 
+        $current_folder = Folder::find($id);
+
+        $tasks = Task::where('folder_id', $current_folder->id)->get();
+
         return view('tasks/index', [
             'folders' => $folders,
             'current_folder_id'=> $id,
+            'tasks' => $tasks,
         ] );
     }
 }
