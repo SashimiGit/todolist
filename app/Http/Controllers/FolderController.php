23de5ff8c1f394fc797c7fe2fc3a8c5d8f39f7ca
Kososhin <27874235+Kososhin@users.noreply.github.com>
2020-06-25 02:43:20 +0800

    Folder_save

--- app/Http/Controllers/FolderController.php
+++ app/Http/Controllers/FolderController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Folder;
 
 class FolderController extends Controller
 {
     //
     public function showCreateform(){
         return view('folders/create');
+    }
+
+    public function create(Request $request){
+        $folder = new Folder();
+
+        $folder->title = $request->title;
+
+        $folder->save();
+
+        return redirect()->route('tasks.index',['id'=> $folder->id,]);
     }
 }
